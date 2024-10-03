0fcb8c27574d6a8dbfae5ea17af1fa46bd8fb586
andreyalex2009 <andreyalex2009@yandex.ru>
2024-10-03 20:42:12 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+the_number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_to_complete_a_single_task = number_of_hours_spent / the_number_of_completed_DZ
+print('Курс : ' + course_name + ', всего задач: ' + str(the_number_of_completed_DZ) + ', затрачено часов: ' + str(number_of_hours_spent) + ', среднее время выполнения ' + str(time_to_complete_a_single_task) + ' часа.')
+
+
+
+
